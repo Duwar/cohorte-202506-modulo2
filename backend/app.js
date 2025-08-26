19bc9cfcd7140b0e8f90f465819c4554d03e28d5
Alexander Rippe <duwar95@gmail.com>
2025-08-25 21:53:53 -0500

    [create] creacion de servidor

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+// El archivo de ejecución de nuestra aplicación
+// configurar nuestro servidor y gestionar la lógica de negocio
+
+    console.log('Hola desarrollador');
+
+//1. Importar las dependencias necesarias
+import express from "express"
+
+//2. configurar las dependencias que necesitamos
+const app = express();
+const port = 3000;
+
+//3. funcionalidades que necesite agregar
+app.get("/",(req,res)=>{
+res.send("El servidor funciona!")
+});
+
+
+//4. levantar el servicio
+app.listen(port, ()=>{
+    console.log(`El servidor esta ejecutandose en http://localhost:${port}`)
+});
